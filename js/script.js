0e11f8d4e35c606972bf7ffb4c4c729c7b735d28
Alexis Zugasti <alexiszugasti@gmail.com>
2022-07-07 18:02:59 -0500

    Added functionality

---
+++ js/script.js
@@ -0,0 +1,42 @@
+let post = document.getElementById("ButtonPost");
+let clear = document.getElementById("ButtonClear");
+let mark = document.getElementById("ButtonMark");
+let del = document.getElementById("ButtonDelete");
+
+post.addEventListener("click", TodoPost);
+clear.addEventListener("click", TodoClear);
+mark.addEventListener("click", TodoMark);
+del.addEventListener("click", TodoDel);
+
+function TodoPost(event) {
+  event.preventDefault();
+
+  //OBtener el texto del textarea
+  let todo = document.getElementById("todoText").value;
+
+  //Referencia del placeholder
+  let list = document.getElementById("todoList");
+
+  let currentList = list.innerHTML;
+  list.innerHTML =
+    currentList + ` <input type = "checkbox" name="todo"/> ${todo} <br>`;
+  document.getElementById("todoText").value = "";
+}
+
+function TodoClear() {
+  let todos = document.getElementsByName("todo");
+  for (let i = 0; i < todos.length; i++) {
+    todos[i].checked = false;
+  }
+}
+
+function TodoMark() {
+  let todos = document.getElementsByName("todo");
+  for (let i = 0; i < todos.length; i++) {
+    todos[i].checked = true;
+}
+
+function TodoDel() {
+  let list = document.getElementById("todoList");
+  list.innerHTML = "";
+}
